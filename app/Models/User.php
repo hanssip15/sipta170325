511ce2917157e7ad9bed303b779559e5e1f5bfd6
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-03-09 17:29:26 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {     
     use HasFactory, Notifiable;
+
     protected $table = 'user';
     protected $primaryKey = 'username';
 
