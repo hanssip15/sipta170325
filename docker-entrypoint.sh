730b8fbf60576809b11793d895f1f93d25beeb63
najibalimudinn <najibaf147@gmail.com>
2025-03-09 14:44:00 +0700

    [Chore] Small changes in entrypoint.sh (message)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ export $(grep -v '^#' .env | xargs)
 
 # Fungsi untuk menunggu database siap
 echo "Menunggu database siap..."
-until php -r "try { new PDO('mysql:host=${DB_HOST};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}'); echo 'Database siap.\n'; } catch (PDOException \$e) { exit(1); }"; do
+until php -r "try { new PDO('mysql:host=${DB_HOST};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}'); echo 'Database siap.'; } catch (PDOException \$e) { exit(1); }"; do
     sleep 3
     echo "Menunggu database..."
 done
