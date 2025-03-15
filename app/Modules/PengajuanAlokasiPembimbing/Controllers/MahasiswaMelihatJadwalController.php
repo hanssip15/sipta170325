996925767d93357153d5fbdf8465923fa9089949
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-03-16 02:38:54 +0700

    [Feature] Frontend for jadwal, still experimenting

--- app/Modules/PengajuanAlokasiPembimbing/Controllers/MahasiswaMelihatJadwalController.php
+++ app/Modules/PengajuanAlokasiPembimbing/Controllers/MahasiswaMelihatJadwalController.php
@@ -11,22 +11,23 @@ class MahasiswaMelihatJadwalController extends Controller
     public function view_MahasiswaMelihatJadwal(): View
     {
 
-        //hardcoded nim
         $nim = 221524049;
 
         $query = "
-        SELECT jadwal_dosen_pembimbing.hari, jadwal_dosen_pembimbing.jam_mulai, jadwal_dosen_pembimbing.jam_selesai FROM `user` 
-JOIN mahasiswa ON `user`.username = mahasiswa.nim
-JOIN kota ON mahasiswa.id_kota=kota.id_kota
-JOIN pengajuan_pembimbing ON pengajuan_pembimbing.id_kota=kota.id_kota
-JOIN alokasi_pembimbing ON alokasi_pembimbing.id_pengajuan_pembimbing=pengajuan_pembimbing.id_pengajuan_pembimbing
-LEFT JOIN jadwal_dosen_pembimbing ON jadwal_dosen_pembimbing.nip=alokasi_pembimbing.nip
-WHERE mahasiswa.nim=$nim AND pengajuan_pembimbing.status_pengajuan='diterima' AND alokasi_pembimbing.status_alokasi='fix'";
+        SELECT 
+            jadwal_dosen_pembimbing.hari, 
+            jadwal_dosen_pembimbing.jam_mulai, 
+            jadwal_dosen_pembimbing.jam_selesai 
+        FROM `user` 
+        JOIN 
+            mahasiswa ON `user`.username = mahasiswa.nim
+            JOIN kota ON mahasiswa.id_kota=kota.id_kota
+            JOIN pengajuan_pembimbing ON pengajuan_pembimbing.id_kota=kota.id_kota
+            JOIN alokasi_pembimbing ON alokasi_pembimbing.id_pengajuan_pembimbing=pengajuan_pembimbing.id_pengajuan_pembimbing
+            LEFT JOIN jadwal_dosen_pembimbing ON jadwal_dosen_pembimbing.nip=alokasi_pembimbing.nip
+        WHERE mahasiswa.nim=$nim AND pengajuan_pembimbing.status_pengajuan='diterima' AND alokasi_pembimbing.status_alokasi='fix'";
         $data = DB::select($query);
 
-        dd($data);
-
-
-        return view('PengajuanAlokasiPembimbing.views.MahasiswaMelihatJadwal.MahasiswaMelihatJadwal');
+        return view('PengajuanAlokasiPembimbing.views.MahasiswaMelihatJadwal.MahasiswaMelihatJadwal', ['data' => $data]);
     }
 }
--- app/Modules/PengajuanAlokasiPembimbing/views/MahasiswaMelihatJadwal/MahasiswaMelihatJadwal.blade.php
+++ app/Modules/PengajuanAlokasiPembimbing/views/MahasiswaMelihatJadwal/MahasiswaMelihatJadwal.blade.php
@@ -12,9 +12,40 @@
     <link href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css" rel="stylesheet" />
 @stop
 
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#jadwalTable').DataTable();
+        });
+    </script>
+@stop
+
 @section('content')
-
     <p>Beranda > <a href="www">Jadwal Dosen Membimbing</a></p>
+    <div class="table-responsive">
+    <table id="jadwalTable" class="table table-bordered">
+    <thead>
+        <tr class="bg-dark text-white">
+            <th class="text-center" style="min-width: 0.5vw;">No</th>
+            <th class="text-center">Hari</th>
+            <th class="text-center" >Jam Awal</th>
+            <th class="text-center">Jam Akhir</th>
+        </tr>
+    </thead>
 
+    <tbody>
+        @foreach ( $data as $item)
+            <tr>
+                <td class="text-center align-middle"> {{ $loop->iteration }} </td>
+                <td class="text-center align-middle"> {{ ucfirst($item->hari) }} </td>
+                <td class="text-center align-middle"> {{ $item->jam_mulai }} </td>
+                <td class="text-center align-middle"> {{ $item->jam_selesai }} </td>
+            </tr>
+        @endforeach
+    </tbody>
+    </div>
 @stop
 
