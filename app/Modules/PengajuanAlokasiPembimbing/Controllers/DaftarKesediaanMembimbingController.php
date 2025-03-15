f410240dcda316c61625c79293022005a137d6a5
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-03-15 21:30:14 +0700

    [Feature] Modifying query dynamicly create and sum kuota membimbing based on prodi, still backend only

--- app/Modules/PengajuanAlokasiPembimbing/Controllers/DaftarKesediaanMembimbingController.php
+++ app/Modules/PengajuanAlokasiPembimbing/Controllers/DaftarKesediaanMembimbingController.php
@@ -13,49 +13,58 @@ class DaftarKesediaanMembimbingController extends Controller
     {
         return view('PengajuanAlokasiPembimbing.views.KesediaanBimbingan.Bidang');
     }
-
     public function view_daftarKesediaanMembimbing(): View
     {
-        $data = DB::select("
-            SELECT 
-                user.nama, 
-                dosen.kode_dosen, 
-                dosen.id_dosen,
-                dosen.nip, 
-                kbk.kbk, 
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 0 
-                    ELSE (dosen.maks_bimbingan_d3 + dosen.maks_bimbingan_d4) 
-                END AS Jumlah_Mhs, 
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 0 
-                    ELSE dosen.maks_bimbingan_d3 
-                END AS Mhs_D3, 
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 0 
-                    ELSE dosen.maks_bimbingan_d4 
-                END AS Mhs_D4,
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 'Belum' 
-                    ELSE 'Sudah' 
-                END AS status_pengumpulan,
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 'Tidak bersedia' 
-                    ELSE 'Bersedia' 
-                END AS kesediaan_d3,
-                CASE 
-                    WHEN COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), '') = '' THEN 'Tidak bersedia' 
-                    ELSE 'Bersedia' 
-                END AS kesediaan_d4,
-                COALESCE(GROUP_CONCAT(bidang.bidang SEPARATOR ', '), 'Tidak memilih bidang') AS bidang_tertarik
-            FROM user
-            JOIN dosen ON user.username = dosen.nip
-            JOIN kbk ON dosen.id_kbk = kbk.id_kbk
-            LEFT JOIN ketertarikan_bidang ON dosen.nip = ketertarikan_bidang.nip
-            LEFT JOIN bidang ON ketertarikan_bidang.id_bidang = bidang.id_bidang
-            GROUP BY dosen.nip, user.nama, dosen.kode_dosen, dosen.id_dosen, kbk.kbk, dosen.maks_bimbingan_d3, dosen.maks_bimbingan_d4
-        ");
+        $prodiList = collect(DB::table('prodi')->pluck('nama_prodi')->toArray());
+    
+        $caseStatements = $prodiList->map(function ($prodi) {
+            return "MAX(CASE WHEN km_grouped.nama_prodi = '$prodi' THEN km_grouped.total_jumlah ELSE 0 END) AS `$prodi`";
+        })->implode(", ");
+    
+        $kesediaanStatements = $prodiList->map(function ($prodi) {
+            return "CASE 
+                        WHEN MAX(CASE WHEN km_grouped.nama_prodi = '$prodi' THEN km_grouped.total_jumlah ELSE 0 END) = 0 
+                        THEN 'Tidak Bersedia' 
+                        ELSE 'Bersedia' 
+                    END AS `Kesediaan_$prodi`";
+        })->implode(", ");
+    
+        $query = 
+        "SELECT d.nip AS nip, 
+            u.nama AS nama,
+            d.kode_dosen AS kode_dosen,
+            d.id_dosen AS id_dosen,
+            k.kbk,
+            CASE 
+                WHEN d.bersedia_membimbing = 'bersedia' THEN 'Sudah' 
+                WHEN d.bersedia_membimbing = 'tidak_bersedia' THEN 'Sudah'
+                ELSE COALESCE(d.bersedia_membimbing, 'Belum Mengumpulkan') 
+            END AS Status_Pengumpulan,
+            COALESCE(GROUP_CONCAT(DISTINCT b.bidang SEPARATOR ', '), '-') AS bidang, 
+            $caseStatements,
+            $kesediaanStatements, 
+            (SELECT SUM(km2.jumlah) FROM kuota_membimbing km2 WHERE km2.nip = d.nip) AS total_jumlah 
+        FROM dosen d
+        LEFT JOIN user u ON d.nip = u.username
+        LEFT JOIN kbk k ON d.id_kbk = k.id_kbk
+        LEFT JOIN ketertarikan_bidang kb ON d.nip = kb.nip
+        LEFT JOIN bidang b ON kb.id_bidang = b.id_bidang
+        LEFT JOIN (
+      
+            SELECT km.nip, p.nama_prodi, SUM(km.jumlah) AS total_jumlah
+            FROM kuota_membimbing km
+            JOIN prodi p ON km.id_prodi = p.id_prodi
+            GROUP BY km.nip, p.nama_prodi
+        ) AS km_grouped ON d.nip = km_grouped.nip
+        GROUP BY d.nip, u.nama, d.kode_dosen, d.id_dosen, k.kbk, d.bersedia_membimbing";
+    
+        $result = DB::select($query);
 
-        return view('PengajuanAlokasiPembimbing.views.DaftarKesediaanMembimbing.DaftarKesediaanMembimbing', compact('data'));
+        $formattedResult = json_decode(json_encode($result), true);
+
+        dd($formattedResult);
+    
+        return view('PengajuanAlokasiPembimbing.views.DaftarKesediaanMembimbing.DaftarKesediaanMembimbing', ['data' => $formattedResult]);
     }
+    
 }
