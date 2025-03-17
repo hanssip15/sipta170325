81d1c2af9773939e9056ae00e168c154db45c60a
Dafa Alfarizki Pratama <Dafaalfarizki33.DA@gmail.com>
2025-03-17 15:14:06 +0700

    Update 2025_03_17_135508_schema_v_3_0_4.php

--- database/migrations/2025_03_17_135508_schema_v_3_0_4.php
+++ database/migrations/2025_03_17_135508_schema_v_3_0_4.php
@@ -1,29 +1,29 @@
-// <?php
+<?php
 
-// use Illuminate\Database\Migrations\Migration;
-// use Illuminate\Database\Schema\Blueprint;
-// use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-// return new class extends Migration
-// {
-//     /**
-//      * Run the migrations.
-//      */
-//     public function up(): void
-//     {
-//         // 1. Modify dosen table, add enum belum_konfirmasi
-//         Schema::table('dosen', function (Blueprint $table) {
-//             $table->enum('bersedia_membimbing', ['bersedia', 'tidak_bersedia', 'belum_konfirmasi'])->default('belum_konfirmasi');
-//         });
-//     }
+return new class extends Migration
+{
+    // /**
+    //  * Run the migrations.
+    //  */
+    // public function up(): void
+    // {
+    //     // 1. Modify dosen table, add enum belum_konfirmasi
+    //     Schema::table('dosen', function (Blueprint $table) {
+    //         $table->enum('bersedia_membimbing', ['bersedia', 'tidak_bersedia', 'belum_konfirmasi'])->default('belum_konfirmasi');
+    //     });
+    // }
 
-//     /**
-//      * Reverse the migrations.
-//      */
-//     public function down(): void
-//     {
-//         Schema::table('dosen', function (Blueprint $table) {
-//             $table->dropColumn('bersedia_membimbing');
-//         });
-//     }
-// };
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::table('dosen', function (Blueprint $table) {
+    //         $table->dropColumn('bersedia_membimbing');
+    //     });
+    // }
+};
